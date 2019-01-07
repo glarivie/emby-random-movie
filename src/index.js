5e18c683d7f4aca4dcc2c563070b793bb4d919b4
Guillaume L <guillaume@skillup.co>
2019-01-07 21:36:47 +0100

    [FIX] request timeout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const random = require('lodash.random')
 const get = require('lodash.get')
-const { log } = require('console')
+const { log, error } = require('console')
 
 const { ticksToMinutes, humanize } = require('./time')
 
@@ -11,14 +11,12 @@ const { EMBY_API_KEY, EMBY_API_ENDPOINT, EMBY_USER_ID } = process.env
 
 const http = axios.create({
   baseURL: EMBY_API_ENDPOINT,
-  timeout: 1000,
-  headers: {
-    'X-Emby-Token': EMBY_API_KEY,
-  },
+  timeout: 10000,
+  headers: { 'X-Emby-Token': EMBY_API_KEY },
 })
 
-const getRandomVideo = async () => {
-  const { data: { Items } } = await http.get(`/Users/${EMBY_USER_ID}/Items`, {
+http
+  .get(`/Users/${EMBY_USER_ID}/Items`, {
     params: {
       SortBy: 'SortName',
       SortOrder: 'Ascending',
@@ -27,18 +25,17 @@ const getRandomVideo = async () => {
       StartIndex: 0,
     },
   })
+  .then(({ data: { Items } }) => {
+    const unseen = Items.filter(movie => !get(movie, 'UserData.Played', false))
+    const { Id, ...movie } = unseen[random(0, unseen.length - 1)]
 
-  const unseen = Items.filter(movie => !get(movie, 'UserData.Played', false))
-  const { Id, ...movie } = unseen[random(0, unseen.length - 1)]
-
-  log({
-    _id: Id,
-    name: get(movie, 'Name', ''),
-    subtitles: get(movie, 'HasSubtitles', false),
-    rating: get(movie, 'CommunityRating', 0),
-    duration: humanize(ticksToMinutes(get(movie, 'RunTimeTicks', 0))),
-    cover: `${EMBY_API_ENDPOINT}/Items/${Id}/Images/Primary`,
+    log({
+      _id: Id,
+      name: get(movie, 'Name', ''),
+      subtitles: get(movie, 'HasSubtitles', false),
+      rating: get(movie, 'CommunityRating', 0),
+      duration: humanize(ticksToMinutes(get(movie, 'RunTimeTicks', 0))),
+      cover: `${EMBY_API_ENDPOINT}/Items/${Id}/Images/Primary`,
+    })
   })
-}
-
-getRandomVideo()
+  .catch(error)
